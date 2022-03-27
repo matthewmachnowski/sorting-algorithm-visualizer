1f3ea41022c6136253ce793903dc642a346521ff
Mateusz Machnowski <ma.machnowsk@student.uw.edu.pl>
2022-03-27 23:50:28 +0200

    updated selection_sort.py

--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -6,9 +6,11 @@ def selection_sort(draw_info, ascending=True):
 
     for i in range(len(lst) - 1):
         minimum = i
+
         for j in range(i + 1, len(lst)):
             if (lst[j] < lst[minimum] and ascending) or (lst[j] > lst[minimum] and not ascending):
                 minimum = j
+
         if minimum != i:
             lst[i], lst[minimum] = lst[minimum], lst[i]
             draw_list(draw_info, {i: draw_info.GREEN, minimum: draw_info.RED}, True)
